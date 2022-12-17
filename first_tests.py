389c4c920e5416531ad46f2c6335bc45009ded14
Ivan-Dyundikov <117164335+Ivan-Dyundikov@users.noreply.github.com>
2022-12-17 18:58:25 +0300

    Add files via upload

---
+++ first_tests.py
@@ -0,0 +1,31 @@
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 9, 3) == 3
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 5, 3) == 2
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 7, 2) == 9
+
+
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_division_calculation_failed(self):
+        assert self.calc.division(self, 9, 3) == 5
+
+    def test_subtraction_calculation_failed(self):
+        assert self.calc.subtraction(self, 6, 3) == 4
+
+    def test_adding_calculation_failed(self):
+        assert self.calc.adding(self, 9, 2) == 1
